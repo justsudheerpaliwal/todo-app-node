1654bfba7a92bf9d736dac5ee995acaf1902828e
sudheer singh <justsudheerpaliwal@gmail.com>
2017-10-21 14:36:54 +0000

    adding required html and css

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ let todoRoutes = require("./routes/todos");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'))
 
 app.use('/api/todos', todoRoutes);
 
-app.listen(port, function(){
-    console.log("APP IS RUNNING ON PORT " + process.env.PORT);
-})
+app.listen(port, (req, res) => {
+    console.log(`server listening on ${port}`);
+});
+
+app.get('/', (req, res) => {
+   res.sendFile('index.html');
+});
     
     
